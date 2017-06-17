041ec0b7ad6ecede12406dc502331608b03cbb5d
Abdul Latheef <abdul.latheef@swiggy.in>
2017-06-17 18:45:41 +0530

    added base code update

--- django_live_log/views.py
+++ django_live_log/views.py
@@ -5,38 +5,43 @@ from django.template import Context, loader
 from django.conf import settings
 
 def log_streamer(request, from_=0, file_path=None):
-    file_path = file_path or settings.DLL_FILE
-    mtime = os.path.getmtime(file_path)
-    with open(file_path) as f:
-        start = -int(from_) or -2000
-        filestart = True
-        while filestart:
-            try:
-                f.seek(start, 2)
-                filestart = False
-                result = f.read()
-                last = f.tell()
-                t = loader.get_template('django_live_log/main.html')
-                yield t.render(Context({"result": result}))
-            except IOError:
-                start += 50
-    reset = 0
-    while True:
-        newmtime = os.path.getmtime(file_path)
-        if newmtime == mtime:
-            time.sleep(1)
-            reset += 1
-            if reset >= 15:
-                yield "<!-- empty -->"
-            continue
-        mtime = newmtime
+    try:
+        file_path = file_path or settings.DLL_FILE
+        mtime = os.path.getmtime(file_path)
         with open(file_path) as f:
-            f.seek(last)
-            result = f.read()
-            if result:
-                t = loader.get_template('django_live_log/main.html')
-                yield result + "<script>$('html,body').animate({ scrollTop: $(document).height() }, 'slow');</script>"
-            last = f.tell()
+            start = -int(from_) or -2000
+            filestart = True
+            while filestart:
+                try:
+                    f.seek(start, 2)
+                    filestart = False
+                    result = f.read()
+                    last = f.tell()
+                    t = loader.get_template('django_live_log/main.html')
+                    yield t.render(Context({"result": result}))
+                except IOError:
+                    start += 50
+        reset = 0
+        while True:
+            newmtime = os.path.getmtime(file_path)
+            if newmtime == mtime:
+                time.sleep(1)
+                reset += 1
+                if reset >= 15:
+                    yield "<!-- empty -->"
+                continue
+            mtime = newmtime
+            with open(file_path) as f:
+                f.seek(last)
+                result = f.read()
+                if result:
+                    t = loader.get_template('django_live_log/main.html')
+                    yield result + "<script>$('html,body').animate({ scrollTop: $(document).height() }, 'slow');</script>"
+                last = f.tell()
+    except Exception as e:
+        import traceback
+        print(traceback.format_exc())
+
 
 def dll(request):
     return StreamingHttpResponse(log_streamer(request, request.GET.get('from', 1000), request.GET.get('file_path', "")))
